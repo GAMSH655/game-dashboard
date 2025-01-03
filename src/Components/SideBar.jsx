3488b82521ad46d18cc8f8b9e96af2d43768d194
BIG_DEV360 <shittuabdquadri0@gmail.com>
2025-01-03 04:13:28 -0800

    wip

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Ps from "../assets/Ps.png"
-import {FaUser} from "react-icons/fa6"
+import {FaUser , FaDashcube} from "react-icons/fa6"
 const Sidebar = () => {
   return (
     <div className="w-20 h-full bg-gray-800 flex flex-col items-center py-6 space-y-6">
@@ -9,6 +9,7 @@ const Sidebar = () => {
       </button>
       <i className="fas fa-gamepad text-gray-400 text-xl"></i>
       <i className="fas fa-comments text-gray-400 text-xl"></i>
+      <FaDashcube className="text-gray-400 text-xl"/>
         <FaUser  className="text-gray-400 text-xl"/> 
     </div>
   );
